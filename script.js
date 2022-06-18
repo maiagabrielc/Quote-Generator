fa314de18e4e1a3cff460f51e56afdddcdaee88f
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-06-17 22:19:35 -0300

    dynamically populating text

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote lenght to determine styling
+  if (quote.text.length > 100) {
+    
+  } else {
+    
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
