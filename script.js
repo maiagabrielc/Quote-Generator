4da641bce4c329b3fa3f17af70d05d16201dcd7b
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-06-17 21:59:20 -0300

    added quote pick and error handling local quote

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
+
 // Get Quotes From API
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes'
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json()
-    console.log(response[0]);
+    newQuote();
   } catch (error) {
+    apiQuotes = localQuotes;
+    newQuote();
     console.log('whops, something is wrong here', error)
   }
 }
